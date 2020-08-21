7a479fb40e949223e499650e192519eeb6f8c69c
unknown <mayank.cit@gmail.com>
2020-08-22 01:40:15 +0530

    Updates

---
+++ precache-manifest.2e4d45bd9bce2f6d3b9f543d98bd7ad4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0491b0addc33514fecf1007f5eab54cd",
+    "url": "/autoamate/index.html"
+  },
+  {
+    "revision": "341a815cd1ae2c0db73b",
+    "url": "/autoamate/static/css/2.4877bccf.chunk.css"
+  },
+  {
+    "revision": "cdea9acd4a3fde9b911c",
+    "url": "/autoamate/static/css/main.2d9bfe79.chunk.css"
+  },
+  {
+    "revision": "341a815cd1ae2c0db73b",
+    "url": "/autoamate/static/js/2.73fd624c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/autoamate/static/js/2.73fd624c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdea9acd4a3fde9b911c",
+    "url": "/autoamate/static/js/main.b484b562.chunk.js"
+  },
+  {
+    "revision": "a985e144eff433fed6af",
+    "url": "/autoamate/static/js/runtime-main.65c6a292.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/autoamate/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/autoamate/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/autoamate/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/autoamate/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/autoamate/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/autoamate/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/autoamate/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/autoamate/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/autoamate/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/autoamate/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
